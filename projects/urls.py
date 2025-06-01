ddf74f5d5ba8f411998be4d316a6090ead5e38d5
Maria Ordóñez <maariiaa1912@gmail.com>
2025-06-01 12:45:44 +0200

    cambios

--- projects/urls.py
+++ projects/urls.py
@@ -3,9 +3,6 @@ from .views import perfil_usuario, token, obtener_menu_semanal
 
 
 urlpatterns = [
-    # path('register/', RegisterView.as_view(), name='register'),
-    # path('login/', LoginView.as_view(), name='login'),
-    # path('logout/', LogoutView.as_view(), name='logout'),
     path('token/', token, name="firestore_token"),
     path('usuarios/<str:uid>/', perfil_usuario, name='perfil_usuario'),
     path('menu/', obtener_menu_semanal, name='menus')
