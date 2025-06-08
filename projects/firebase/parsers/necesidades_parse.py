ce6056865f6a25dfff406300ec744465c717cfff
Maria Ordóñez <maariiaa1912@gmail.com>
2025-06-08 15:07:05 +0200

    quitamos una coma para hacer bien el parse

--- projects/firebase/parsers/necesidades_parse.py
+++ projects/firebase/parsers/necesidades_parse.py
@@ -3,7 +3,7 @@ from projects.firebase.helpers import convertir_fecha_utc_a_local
 
 def parse_necesidad_document(doc):
     fields = doc.get("fields", {})
-    fecha_raw = fields.get("fecha", {}).get("timestampValue", ""),
+    fecha_raw = fields.get("fecha", {}).get("timestampValue", "")
     return {
         "id": doc["name"].split("/")[-1],
         "fecha": convertir_fecha_utc_a_local(fecha_raw),
